2a6a9cd0eb774ffcea354390e7e3a9b976c46c36
WinnieLOurson <zefzack@outlook.fr>
2018-05-01 15:19:52 +0200

    maj add des niveux mais c pâs top

--- Try 2 Survive/oui.js
+++ Try 2 Survive/oui.js
@@ -2,7 +2,18 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
 
-    game.load.image('button', 'img/smiley.jpg');
+    game.load.image('button 1.1', 'img/smiley.jpg');
+    game.load.image('button 1.2', 'img/smiley.jpg');
+    game.load.image('button 1.3', 'img/smiley.jpg');
+    game.load.image('button 1.4', 'img/smiley.jpg');
+    game.load.image('button 2.1', 'img/smiley.jpg');
+    game.load.image('button 2.2', 'img/smiley.jpg');
+    game.load.image('button 2.3', 'img/smiley.jpg');
+    game.load.image('button 2.4', 'img/smiley.jpg');
+    game.load.image('button 3.1', 'img/coeur.jpg');
+    game.load.image('button 3.2', 'img/coeur.jpg');
+    game.load.image('button 3.3', 'img/coeur.jpg');
+    game.load.image('button 3.4', 'img/coeur.jpg');
 
 }
 
@@ -18,8 +29,14 @@ var button2;
 var button3;
 var button4;
 
+var buttonA;
+var buttonB;
+var buttonC;
+var buttonD;
+
 var score = 0;
 var reussite = 0;
+var level = 0
 
 function requete (ordre){
     
@@ -45,26 +62,63 @@ function update () {
     
 }
 
-function create() {;
-
-    //  Standard button (also used as our pointer tracker)
-    button1 = game.add.button(100, 100, 'button', actionOnClick1, this, 2, 1, 0);
+function create() {
+    if (score >= 20){
+            //  Standard button (also used as our pointer tracker)
+    button1 = game.add.button(100, 100, 'button 1.1', actionOnClick1, this, 2, 1, 0);
     button1.anchor.setTo(0.5, 0.5);
 
 
     //  Rotated button
-    button2 = game.add.button(100, 500, 'button', actionOnClick3, this, 2, 1, 0);
+    button2 = game.add.button(100, 500, 'button 1.2', actionOnClick3, this, 2, 1, 0);
     button2.angle = 24;
     button2.anchor.setTo(0.5, 0.5);
 
     //  Standard button (also used as our pointer tracker)
-    button3 = game.add.button(500, 100, 'button', actionOnClick2, this, 2, 1, 0);
+    button3 = game.add.button(500, 100, 'button 1.3' , actionOnClick2, this, 2, 1, 0);
     button3.anchor.setTo(0.5, 0.5);
 
     //  Standard button (also used as our pointer tracker)
-    button4 = game.add.button(500, 500, 'button', actionOnClick4, this, 2, 1, 0);
+    button4 = game.add.button(500, 500, 'button 1.4' , actionOnClick4, this, 2, 1, 0);
+    button4.anchor.setTo(0.5, 0.5);
+} else if (score >= 10){
+    //  Standard button (also used as our pointer tracker)
+button1 = game.add.button(100, 100, 'button 2.1', actionOnClick1, this, 2, 1, 0);
+button1.anchor.setTo(0.5, 0.5);
+
+//  Rotated button
+button2 = game.add.button(100, 500, 'button 2.2', actionOnClick3, this, 2, 1, 0);
+button2.angle = 24;
+button2.anchor.setTo(0.5, 0.5);
+
+//  Standard button (also used as our pointer tracker)
+button3 = game.add.button(500, 100, 'button 2.3' , actionOnClick2, this, 2, 1, 0);
+button3.anchor.setTo(0.5, 0.5);
+
+//  Standard button (also used as our pointer tracker)
+button4 = game.add.button(500, 500, 'button 2.4' , actionOnClick4, this, 2, 1, 0);
+button4.anchor.setTo(0.5, 0.5);
+}
+            else (score >= 0) 
+                   //  Standard button (also used as our pointer tracker)
+    button1 = game.add.button(100, 100, 'button 3.1', actionOnClick1, this, 2, 1, 0);
+    button1.anchor.setTo(0.5, 0.5);
+
+
+    //  Rotated button
+    button2 = game.add.button(100, 500, 'button 3.2', actionOnClick3, this, 2, 1, 0);
+    button2.angle = 24;
+    button2.anchor.setTo(0.5, 0.5);
+
+    //  Standard button (also used as our pointer tracker)
+    button3 = game.add.button(500, 100, 'button 3.3' , actionOnClick2, this, 2, 1, 0);
+    button3.anchor.setTo(0.5, 0.5);
+
+    //  Standard button (also used as our pointer tracker)
+    button4 = game.add.button(500, 500, 'button 3.4' , actionOnClick4, this, 2, 1, 0);
     button4.anchor.setTo(0.5, 0.5);
 }
+
 
 function up() {
     console.log('button up', arguments);
